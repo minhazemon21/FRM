d9005fecc2fa7a91f59a0c69d6d0db6219c30b62
minhazemon21 <minhazkhondokar21@gmail.com>
2023-03-01 15:42:05 +0600

    d

--- joining learn.sql
+++ joining learn.sql
@@ -19,3 +19,37 @@ SELECT MST.SubledgerMasterName, DTL.SubledgerDetailsName
 FROM AccSubledgerDetails AS DTL RIGHT JOIN AccSubledgerMaster AS MST ON
 DTL.AccSubledgerMasterId = MST.Id
 
+select * from EMP_PROFILE 
+select 
+p.emp_name
+, p.OfficeEmail
+, o.BranchName
+, p.MobileNoConvence
+, d.DesignationName
+, dpt.DepartmentName
+, c.CountryName
+, ci.CompanyName
+, cu.CurrencyName
+, isnull(s.PayableAmount,0) PayableAmount
+from EMP_PROFILE p
+inner join LookupDesignation d on p.emp_desg_id = d.Id
+inner join BrokerDepartment dpt on p.emp_dept_id = dpt.Id
+inner join LookupCountry c on dpt.CompanyId = c.Id
+inner join CompanyInformation ci on p.CompanyId = ci.Id
+inner join LookupCurrency cu on p.currency_id = cu.Id
+inner join SalaryMaster s on p.emp_id = s.Id
+inner join OfficeBranch o on p.emp_branch_id = o.Id
+
+select 
+o.Name
+, o.Income
+--, m.Id
+--, s.Id
+, s.Expense
+, sum(m.Amount) as MnthlyExAmount
+, s.Expense + sum(m.Amount) as TotalExp
+, o.Income - (s.Expense + sum(m.Amount)) as CurrentBalance
+from OBroInfo o
+left join MonthlyExp m on o.ID = m.Id
+inner join Status s on o.Status = s.Id
+group by m.Id, o.ID, o.Name, o.Income, s.Id, s.Expense
