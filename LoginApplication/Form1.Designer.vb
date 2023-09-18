63572bb769c4691ad6d8c0ce08b6b91e7d5f0fc3
minhazemon21 <minhazkhondokar21@gmail.com>
2023-09-18 10:47:56 +0600

    log in

--- LoginApplication/Form1.Designer.vb
+++ LoginApplication/Form1.Designer.vb
@@ -63,7 +63,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(396, 225)
+        Me.Button1.Location = New System.Drawing.Point(396, 226)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 4
--- LoginApplication/Form1.vb
+++ LoginApplication/Form1.vb
@@ -1,4 +1,7 @@
-﻿Public Class Form1
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -9,5 +12,18 @@
 
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim con As SqlConnection = New SqlConnection("Data Source=DESKTOP-QB4O65F\SQLEXPRESS;Initial Catalog=LoginDB;Integrated Security=True")
+        Dim cmd As SqlCommand = New SqlCommand("SELECT * FROM LoginForm WHERE Username='" + txtUserName.Text + "'AND Password='" + txtPassword.Text + "'", con)
+        Dim sda As SqlDataAdapter = New SqlDataAdapter(cmd)
+        Dim dt As DataTable = New DataTable()
+        sda.Fill(dt)
+        If (dt.Rows.Count > 0) Then
+            MessageBox.Show("Login Success", "information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Error", "information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
     End Sub
 End Class
